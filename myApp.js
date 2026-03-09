57f0484abfeab7a83b2831ce50616bc31e076352
DarkPhoenixrise <tanishq6127@gmail.com>
2026-03-09 23:58:05 +0530

    fixed myApp

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
 let express = require('express');
 let app = express();
+
 console.log("Hello World");
 
+app.get("/", function(req, res) {
+  res.send("Hello from the server");
+});
 
-
-
-
-
+module.exports = app;
 
 
 
