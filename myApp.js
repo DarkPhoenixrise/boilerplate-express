d492e2c0a381ab38e5eeacca94bec8043f2a416b
DarkPhoenixrise <tanishq6127@gmail.com>
2026-03-10 00:12:57 +0530

    fixed middleware

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ let abspath=__dirname+"/views/index.html";
 app.get("/", function(req, res) {
   res.sendFile(abspath);
 });
-app.use(express.static(__dirname+"/public/style.css"));
+app.use(express.static(__dirname+"/public"));
 module.exports = app;
 
 
