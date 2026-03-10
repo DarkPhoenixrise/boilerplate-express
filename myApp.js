0bf0a1d13807d893a121e02dee863f1b7dc24050
DarkPhoenixrise <tanishq6127@gmail.com>
2026-03-10 20:20:11 +0530

    add chain mount

--- myApp.js
+++ myApp.js
@@ -8,9 +8,21 @@ let abspath = __dirname + "/views/index.html";
 
 app.use(function(req,res,next){
     let string=req.method+" "+req.path+" - "+req.ip;
-    console.log(string);
+    res.send({
+        "string": string
+    });
     next();
 });
+app.get("/now",function(req,res,next){
+    req.time=new Date().toString();
+    next();
+},
+function(req,res){
+    res.send({
+        "time":req.time
+    });
+}
+);
 app.get("/", function(req, res) {
   res.sendFile(abspath);
 });
